e394cf5ac45834ecf84e582ce2ddae93405247bc
p4songer <pivsonger@gmail.com>
2026-02-01 16:38:56 -0700

    Got Circle Spin Game
    Now have a thing to track if you spun your mouse in a circle.

--- gdkor-1/Temp/star_minigame.gd
+++ gdkor-1/Temp/star_minigame.gd
@@ -1,4 +1,17 @@
 extends Node2D
 
+var default_sequence : Array = [
+	"dextrin", "water / start", "color", "mix", "oxidizer", "mix", "press"
+]
+var crackle_sequence : Array = [
+	"dextrin", "water / start", "color", "oxidizer", "mix", "divider", "oxidizer",
+	"mix", "divider", "oxidizer", "mix", "press"
+]
+var brocade_sequence : Array = [
+	"dextrin", "water/start", "color", "charcoal", "oxidizer", "mix", "charcoal", "mix", "press"
+]
+var palm_sequence : Array = [
+	"dextrin", "water/start", "color", "oxidizer", "mix", "wrap", "glue", "mix"
+]
 func _ready() -> void:
 	pass
---
+++ gdkor-1/Temp/test_rotate.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@onready var obj: Node2D = $obj
+var dir_array : Array
+
+func _ready() -> void:
+	for i in $points.get_child_count():
+		$points.get_child(i).area_entered.connect(_on_point_entered.bind(i))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	obj.look_at(get_global_mouse_position())
+
+
+func _on_point_entered(_area : Area2D, idx: int) -> void:
+	dir_array.append(idx)
+	if dir_array.size() < 4: return
+	# if we have 4 elements, check the sequence.
+	if dir_array.size() > 4:
+		dir_array.remove_at(0)
+	var total = 0
+	for i in dir_array:
+		total += i
+	if total == 6:
+		print("correct")
+		#TODO emit signal here.
+		dir_array.clear()
