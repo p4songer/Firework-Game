175cf7f3c54ef22d16ebc836988045947059161c
p4songer <pivsonger@gmail.com>
2026-08-11 22:22:48 -0600

    feat: Shop Screen
    Shop screen is up and buttons work. Need to connect to signals and actually set up coms

--- firework-game/Main/House Scenes/Shop/shop_button.gd
+++ firework-game/Main/House Scenes/Shop/shop_button.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 	for c in selection.get_children():
 		if c is Button:
 			c.pressed.connect(_on_button_pressed.bind(c.name))
-	self.pressed.connect(_on_purchase_pressed)
+	$Icon.pressed.connect(_on_purchase_pressed)
 
 
 func _on_button_pressed(button_name: String) -> void:
